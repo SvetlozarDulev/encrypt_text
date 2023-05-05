a2b124b33383414d12e2423de1abd9a524ebe9ab
SvetlozarDulev <pr_hack@protonmail.com>
2023-05-05 21:37:22 +0300

    Encrypting

---
+++ encrypting_text.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+f = Fernet(key)
+
+text = input("Input text for encryption: ")
+text_to_bytes = text.encode()
+
+encrypt = f.encrypt(text_to_bytes)
+print(f"Encrypted result: {encrypt}")
+
+decrypt = f.decrypt(encrypt)
+print(f"Decrypted result: {decrypt}")
+
